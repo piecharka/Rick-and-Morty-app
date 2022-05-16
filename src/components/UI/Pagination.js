84bdd6d5bbd935f23ac136a2f82cb0c42094cc13
piecharka <kowalas02.kk@gmail.com>
2022-05-16 09:32:57 +0200

    pagination commit

---
+++ src/components/UI/Pagination.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import classes from "./Pagination.module.css";
+const Pagination = (props) => {
+  const [page, setPage] = useState(1);
+  const pageChangeHandler = (e) => {
+    console.log(e.target.value);
+    setPage(e.target.value);
+    props.changePage(e.target.value);
+  };
+  return (
+    <div>
+      <form className={classes.pagination_form}>
+        <label>Page:</label>
+        <input
+          type="number"
+          value={page}
+          onChange={pageChangeHandler}
+          min={1}
+          max={42}
+        />
+      </form>
+    </div>
+  );
+};
+
+export default Pagination;
--- src/pages/CharacterPage.js
+++ src/pages/CharacterPage.js
@@ -1,16 +1,19 @@
 import { CircularProgress } from "@mui/material";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import CharacterList from "../components/CharacterList/CharacterList";
+import Pagination from "../components/UI/Pagination";
 import useFetch from "../hooks/use-fetch";
 
 const CharacterPage = () => {
+  const [page, setPage] = useState(1);
   const [data, isLoading] = useFetch(
-    "https://rickandmortyapi.com/api/character/?page=1",
+    `https://rickandmortyapi.com/api/character/?page=${page}`,
     false
   );
 
   return (
     <Fragment>
+      <Pagination changePage={setPage} />
       {isLoading && <CircularProgress className="isLoading" />}
       {!isLoading && <CharacterList data={data.results} />}
     </Fragment>
